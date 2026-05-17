7bd0359bab9a001ad905b09165c7516e79e9432f
otkpetare100-ux <otkpetare100@gmail.com>
2026-05-17 11:37:58 -0400

    fix: solve temporal dead zone hoisting issue by declaring spell and rune maps globally

--- web/main.js
+++ web/main.js
@@ -4,6 +4,48 @@ const API_BASE = `${window.location.origin}/api`;
 const ASSETS_BASE = '/assets';
 let DDRAGON_VERSION = '14.9.1';
 
+// Mapeos estáticos globales de Hechizos e Iconos de Runas para el Historial
+const SUMMONER_SPELL_MAP = {
+  1: "SummonerBoost",      // Purificar
+  3: "SummonerExhaust",    // Extenuación
+  4: "SummonerFlash",      // Destello
+  6: "SummonerHaste",      // Fantasma
+  7: "SummonerHeal",       // Curar
+  11: "SummonerSmite",     // Aplastar
+  12: "SummonerTeleport",  // Teleportar
+  14: "SummonerDot",       // Ignición
+  21: "SummonerBarrier",   // Barrera
+  32: "SummonerSnowball",  // Bola de nieve (Mark ARAM)
+};
+
+const KEYSTONE_RUNE_MAP = {
+  8005: "perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png",
+  8008: "perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png",
+  8010: "perk-images/Styles/Precision/Conqueror/Conqueror.png",
+  8021: "perk-images/Styles/Precision/FleetFootwork/FleetFootwork.png",
+  8112: "perk-images/Styles/Domination/Electrocute/Electrocute.png",
+  8124: "perk-images/Styles/Domination/Predator/Predator.png",
+  8128: "perk-images/Styles/Domination/DarkHarvest/DarkHarvest.png",
+  9923: "perk-images/Styles/Domination/HailOfBlades/HailOfBlades.png",
+  8214: "perk-images/Styles/Sorcery/SummonAery/SummonAery.png",
+  8229: "perk-images/Styles/Sorcery/ArcaneComet/ArcaneComet.png",
+  8230: "perk-images/Styles/Sorcery/PhaseRush/PhaseRush.png",
+  8437: "perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png",
+  8439: "perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png",
+  8465: "perk-images/Styles/Resolve/Guardian/Guardian.png",
+  8351: "perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png",
+  8360: "perk-images/Styles/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png",
+  8369: "perk-images/Styles/Inspiration/FirstStrike/FirstStrike.png"
+};
+
+const RUNE_STYLE_MAP = {
+  8000: "perk-images/Styles/7201_Precision.png",
+  8100: "perk-images/Styles/7200_Domination.png",
+  8200: "perk-images/Styles/7202_Sorcery.png",
+  8300: "perk-images/Styles/7203_Inspiration.png",
+  8400: "perk-images/Styles/7204_Resolve.png"
+};
+
 // Controladores de la barra de carga minimalista dorado premium
 let loadingBarTimer = null;
 
@@ -1247,48 +1289,6 @@ function openPlayerDetails(player) {
       `;
     });
   }
-
-  // Mapeos estáticos de Hechizos e Iconos de Runas
-  const SUMMONER_SPELL_MAP = {
-    1: "SummonerBoost",      // Purificar
-    3: "SummonerExhaust",    // Extenuación
-    4: "SummonerFlash",      // Destello
-    6: "SummonerHaste",      // Fantasma
-    7: "SummonerHeal",       // Curar
-    11: "SummonerSmite",     // Aplastar
-    12: "SummonerTeleport",  // Teleportar
-    14: "SummonerDot",       // Ignición
-    21: "SummonerBarrier",   // Barrera
-    32: "SummonerSnowball",  // Bola de nieve (Mark ARAM)
-  };
-
-  const KEYSTONE_RUNE_MAP = {
-    8005: "perk-images/Styles/Precision/PressTheAttack/PressTheAttack.png",
-    8008: "perk-images/Styles/Precision/LethalTempo/LethalTempoTemp.png",
-    8010: "perk-images/Styles/Precision/Conqueror/Conqueror.png",
-    8021: "perk-images/Styles/Precision/FleetFootwork/FleetFootwork.png",
-    8112: "perk-images/Styles/Domination/Electrocute/Electrocute.png",
-    8124: "perk-images/Styles/Domination/Predator/Predator.png",
-    8128: "perk-images/Styles/Domination/DarkHarvest/DarkHarvest.png",
-    9923: "perk-images/Styles/Domination/HailOfBlades/HailOfBlades.png",
-    8214: "perk-images/Styles/Sorcery/SummonAery/SummonAery.png",
-    8229: "perk-images/Styles/Sorcery/ArcaneComet/ArcaneComet.png",
-    8230: "perk-images/Styles/Sorcery/PhaseRush/PhaseRush.png",
-    8437: "perk-images/Styles/Resolve/GraspOfTheUndying/GraspOfTheUndying.png",
-    8439: "perk-images/Styles/Resolve/VeteranAftershock/VeteranAftershock.png",
-    8465: "perk-images/Styles/Resolve/Guardian/Guardian.png",
-    8351: "perk-images/Styles/Inspiration/GlacialAugment/GlacialAugment.png",
-    8360: "perk-images/Styles/Inspiration/UnsealedSpellbook/UnsealedSpellbook.png",
-    8369: "perk-images/Styles/Inspiration/FirstStrike/FirstStrike.png"
-  };
-
-  const RUNE_STYLE_MAP = {
-    8000: "perk-images/Styles/7201_Precision.png",
-    8100: "perk-images/Styles/7200_Domination.png",
-    8200: "perk-images/Styles/7202_Sorcery.png",
-    8300: "perk-images/Styles/7203_Inspiration.png",
-    8400: "perk-images/Styles/7204_Resolve.png"
-  };
 
   // Render Historial de Partidas
   function renderHistory(history, queueType) {
